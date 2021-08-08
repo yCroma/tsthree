550cd82108351aaeb7baf77e102a7a61d93f15e6
yCroma <58209438+yCroma@users.noreply.github.com>
2021-08-09 00:20:44 +0900

    ADD: index.ts

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import * as THREE from "three";
+import { GUI } from "dat.gui";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer();
+  // レンダラーのサイズを設定
+  renderer.setSize(800, 600);
+  // canvasをbodyに追加
+  document.body.appendChild(renderer.domElement);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 10000);
+  camera.position.set(0, 0, 1000);
+
+  // 箱を作成
+  const geometry = new THREE.BoxGeometry(250, 250, 250);
+  const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+  const box = new THREE.Mesh(geometry, material);
+  box.position.z = -5;
+  scene.add(box);
+
+  // 平行光源を生成
+  const light = new THREE.DirectionalLight(0xffffff);
+  light.position.set(1, 1, 1);
+  scene.add(light);
+
+  // dat.gui
+  const gui: GUI = new GUI();
+  const LightFolder: GUI = gui.addFolder("Light");
+  const LightFolderParam: Object = {
+    Light1: "Hello",
+    addLight: "AddLight",
+    help: "test",
+  };
+
+  LightFolder.add(LightFolderParam, "Light1", "adder");
+
+  const tick = (): void => {
+    requestAnimationFrame(tick);
+
+    box.rotation.x += 0.05;
+    box.rotation.y += 0.05;
+
+    // 描画
+    renderer.render(scene, camera);
+  };
+  tick();
+
+  console.log("Hello Three.js");
+});
