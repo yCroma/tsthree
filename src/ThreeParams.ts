a02cf66bda5e187ff02304a50b0585fed690cc47
yCroma <58209438+yCroma@users.noreply.github.com>
2021-08-14 03:13:24 +0900

    ADD: interface ThreeParams

---
+++ src/ThreeParams.ts
@@ -0,0 +1,23 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+export interface ThreeParams {
+  /**
+   * カメラとシーンはレンダリングに必要なため、必ず宣言する.
+   */
+
+  /**
+   * カメラは単体のみを扱う.
+   * 複数扱いたい場合は、Camara[] で宣言すること
+   */
+  Cameras?: Camera | undefined;
+  /**
+   * プロトタイプでは, コントローラーの切り替えはサポートしない.
+   */
+  Control?: Control | undefined;
+  Lights?: Light[] | undefined;
+}
+
+type Camera = THREE.PerspectiveCamera | THREE.StereoCamera;
+type Control = OrbitControls;
+type Light = THREE.AmbientLight;
